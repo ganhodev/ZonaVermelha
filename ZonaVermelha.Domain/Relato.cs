48de383e176f607f2b15c66821f392c60aee826b
Guilherme Ganho <guilhermeganho2208@gmail.com>
2026-07-07 16:58:58 -0300

    feat: adiciona classes base ao arquivo Domain

---
+++ ZonaVermelha.Domain/Relato.cs
@@ -0,0 +1,14 @@
+﻿namespace ZonaVermelha.Domain;
+
+public class Relato
+{
+    //criar identificadores únicos
+    public Guid IdRelato { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public double Latitude { get; set; } = double.NaN;
+    public double Longitude { get; set; } = double.NaN;
+    public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+    public Guid UsuarioId { get; set; }
+    public Guid ZonaId { get; set; }
+    public Zona Zona { get; set; } = null!;
+}
---
+++ ZonaVermelha.Domain/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ZonaVermelha.Domain;
+
+public class Usuario
+{
+    public Guid IdUsuario{ get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public ICollection<Relato> Relatos { get; set; } = [];
+}
