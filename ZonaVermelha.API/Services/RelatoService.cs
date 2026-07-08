a656a8286119b68094ce36ac9b4725fca77e3917
Guilherme Ganho <guilhermeganho2208@gmail.com>
2026-07-08 20:55:33 -0300

    feat: implementa logica de criação de relato e agrupamento em zonas
    Adiciona RelatoService.CriarRelatoAsync, que busca zonas
    existentes, calcula distancia via Haversine, e decide entre
    associar o relato a uma zona proxima ou criar uma nova.

--- ZonaVermelha.API/Services/RelatoService.cs
+++ ZonaVermelha.API/Services/RelatoService.cs
@@ -1,6 +1,8 @@
 ﻿using ZonaVermelha.Communication.Requests;
 using ZonaVermelha.Communication.Responses;
 using ZonaVermelha.Infrastructure;
+ using Microsoft.EntityFrameworkCore;
+using ZonaVermelha.Domain;
 
 namespace ZonaVermelha.API.Services;
 //async/await permite que a thread não fique bloqueada esperando.
@@ -8,6 +10,57 @@ public class RelatoService(ZonaVermelhaDbContext dbContext)
 {
     public async Task<ResponseRelatoJson> CriarRelatoAsync(RequestRelatoJson requestRelatoJson)
     {
-        throw new NotImplementedException();
+        var zonas = await dbContext.Zonas.ToListAsync();
+
+        //Aqui fica um objeto anônimo com { Zona, Distancia }, se encontrou alguma zona dentro do raio
+        //null, se nenhuma zona satisfez a condição do .Where()
+
+        var zonaMaisProxima = zonas
+            .Select(z => new
+            {
+                Zona = z,
+                Distancia = CalculadoraDistancia.CalcularDistanciaMetros(
+                    requestRelatoJson.Latitude,
+                    requestRelatoJson.Longitude,
+                    z.Latitude,
+                    z.Longitude)
+            })
+            .Where(d => d.Distancia < d.Zona.RaioMetros)
+            .OrderBy(d => d.Distancia).FirstOrDefault();
+
+        Zona zonaFinal;
+
+        if (zonaMaisProxima == null)
+        {
+            zonaFinal = new Zona(requestRelatoJson.Latitude, requestRelatoJson.Longitude)
+            {
+                RaioMetros = 200,
+                NivelIntensidadeZona = 1,
+                CriadaEm = DateTime.UtcNow,
+                UltimaAtividade = DateTime.UtcNow
+            };
+            dbContext.Zonas.Add(zonaFinal);
+        } else
+        {
+            zonaFinal = zonaMaisProxima.Zona;
+            zonaFinal.NivelIntensidadeZona++;
+            zonaFinal.UltimaAtividade = DateTime.UtcNow;
+        }
+        var relato = new Relato(requestRelatoJson.Descricao, requestRelatoJson.Latitude, requestRelatoJson.Longitude, requestRelatoJson.UsuarioId);
+        relato.ZonaId = zonaFinal.IdZona;
+
+        dbContext.Relatos.Add(relato);
+
+        await dbContext.SaveChangesAsync();
+
+        return new ResponseRelatoJson
+        {
+            IdRelato = relato.IdRelato,
+            Descricao = relato.Descricao,
+            Latitude = relato.Latitude,
+            Longitude = relato.Longitude,
+            CriadoEm = relato.CriadoEm,
+            ZonaId = relato.ZonaId
+        };
     }
 }
